3bd59113e14abf31d08bbf3b7d70da45959b9df9
Pierre lulu <luttmannpierre@gmail.com>
2024-11-20 11:33:38 +0100

    First commit of tcp com$

---
+++ TcpIp/TcpIp/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace TcpIp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbCom = new System.Windows.Forms.TextBox();
+            this.boutClient = new System.Windows.Forms.Button();
+            this.boutServeur = new System.Windows.Forms.Button();
+            this.boutQuit = new System.Windows.Forms.Button();
+            this.tbMessage = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbCom
+            // 
+            this.tbCom.Location = new System.Drawing.Point(20, 27);
+            this.tbCom.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbCom.Multiline = true;
+            this.tbCom.Name = "tbCom";
+            this.tbCom.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbCom.Size = new System.Drawing.Size(262, 270);
+            this.tbCom.TabIndex = 0;
+            // 
+            // boutClient
+            // 
+            this.boutClient.Location = new System.Drawing.Point(359, 56);
+            this.boutClient.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.boutClient.Name = "boutClient";
+            this.boutClient.Size = new System.Drawing.Size(56, 19);
+            this.boutClient.TabIndex = 1;
+            this.boutClient.Text = "Client";
+            this.boutClient.UseVisualStyleBackColor = true;
+            this.boutClient.Click += new System.EventHandler(this.boutClient_Click);
+            // 
+            // boutServeur
+            // 
+            this.boutServeur.Location = new System.Drawing.Point(359, 27);
+            this.boutServeur.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.boutServeur.Name = "boutServeur";
+            this.boutServeur.Size = new System.Drawing.Size(56, 19);
+            this.boutServeur.TabIndex = 2;
+            this.boutServeur.Text = "Serveur";
+            this.boutServeur.UseVisualStyleBackColor = true;
+            this.boutServeur.Click += new System.EventHandler(this.boutServeur_Click);
+            // 
+            // boutQuit
+            // 
+            this.boutQuit.Location = new System.Drawing.Point(359, 191);
+            this.boutQuit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.boutQuit.Name = "boutQuit";
+            this.boutQuit.Size = new System.Drawing.Size(56, 19);
+            this.boutQuit.TabIndex = 3;
+            this.boutQuit.Text = "Quitter";
+            this.boutQuit.UseVisualStyleBackColor = true;
+            this.boutQuit.Click += new System.EventHandler(this.boutQuit_Click);
+            // 
+            // tbMessage
+            // 
+            this.tbMessage.Location = new System.Drawing.Point(20, 328);
+            this.tbMessage.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbMessage.Name = "tbMessage";
+            this.tbMessage.Size = new System.Drawing.Size(262, 20);
+            this.tbMessage.TabIndex = 4;
+            this.tbMessage.Text = "Ceci est un message test";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 392);
+            this.Controls.Add(this.tbMessage);
+            this.Controls.Add(this.boutQuit);
+            this.Controls.Add(this.boutServeur);
+            this.Controls.Add(this.boutClient);
+            this.Controls.Add(this.tbCom);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbCom;
+        private System.Windows.Forms.Button boutClient;
+        private System.Windows.Forms.Button boutServeur;
+        private System.Windows.Forms.Button boutQuit;
+        private System.Windows.Forms.TextBox tbMessage;
+    }
+}
+
---
+++ TcpIp/TcpIp/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TcpIp
+{
+    public partial class Form1 : Form
+    {
+
+        private IPAddress m_ipAdrServeur;
+        private IPAddress m_ipAdrClient;
+        private int m_numPort;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            m_ipAdrServeur = IPAddress.Parse("169.254.161.209");  // Adresse locale
+            m_ipAdrClient = IPAddress.Parse("192.168.62.109");   // Adresse distante
+            m_numPort = 8001;
+        }
+
+        private void boutClient_Click(object sender, System.EventArgs e)
+        {
+            TcpClient tcpClient = new TcpClient();
+            this.tbCom.AppendText("Connexion en cours...\r\n");
+
+            tcpClient.Connect(m_ipAdrClient, m_numPort);
+            this.tbCom.AppendText("Connexion établie\r\n");
+
+            // Charger l'image depuis un fichier
+            string imagePath = "path_to_image.png"; // Remplacez par le chemin de votre image
+            byte[] imageBytes = File.ReadAllBytes(imagePath);
+
+            // Envoyer la taille de l'image
+            NetworkStream stream = tcpClient.GetStream();
+            byte[] sizeInfo = BitConverter.GetBytes(imageBytes.Length);
+            stream.Write(sizeInfo, 0, sizeInfo.Length);
+
+            // Envoyer l'image
+            stream.Write(imageBytes, 0, imageBytes.Length);
+            this.tbCom.AppendText($"Image envoyée : {imagePath}\r\n");
+
+            tcpClient.Close();
+        }
+
+        private void boutServeur_Click(object sender, System.EventArgs e)
+        {
+            TcpListener tcpList = new TcpListener(m_ipAdrServeur, m_numPort);
+            tcpList.Start();
+            this.tbCom.AppendText("Serveur en cours d'exécution...\r\n");
+            this.tbCom.AppendText("Attente de connexion...\r\n");
+
+            Socket sock = tcpList.AcceptSocket();
+            this.tbCom.AppendText($"Connexion acceptée de {sock.RemoteEndPoint}\r\n");
+
+            NetworkStream stream = new NetworkStream(sock);
+
+            // Lire la taille de l'image
+            byte[] sizeInfo = new byte[4];
+            stream.Read(sizeInfo, 0, sizeInfo.Length);
+            int imageSize = BitConverter.ToInt32(sizeInfo, 0);
+
+            // Lire l'image
+            byte[] imageBytes = new byte[imageSize];
+            int bytesRead = 0;
+            while (bytesRead < imageSize)
+            {
+                bytesRead += stream.Read(imageBytes, bytesRead, imageSize - bytesRead);
+            }
+
+            // Sauvegarder l'image reçue
+            string outputPath = "received_image.png"; // Chemin de sauvegarde
+            File.WriteAllBytes(outputPath, imageBytes);
+            this.tbCom.AppendText($"Image reçue et sauvegardée : {outputPath}\r\n");
+
+            tcpList.Stop();
+            sock.Close();
+        }
+
+        private void boutQuit_Click(object sender, System.EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
+
